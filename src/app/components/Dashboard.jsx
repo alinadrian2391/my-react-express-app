2f874202792d84b9fafebe9975d1b1338a1f3d3c
Alin Perez <alinperez@Alins-MacBook-Air.local>
2021-05-14 14:16:15 -0400

    first commit

---
+++ src/app/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Dashboard = ({groups}) => {
+    return (
+        <div>
+            <h2>DashBoard</h2>
+        </div>
+    )
+}
+
+
---
+++ src/app/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Dashboard} from './components/Dashboard.jsx'
+import {store} from "../store";
+
+ReactDOM.render(<Dashboard/>, document.getElementById("app"));
+
